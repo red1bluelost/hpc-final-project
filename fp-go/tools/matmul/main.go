8d8fe1b82ce09078d8491b7de4181f5b12d0c11c
Micah Weston <msweston12@cox.net>
2022-03-23 12:48:44 -0400

    Adds timing to the matrix multiply.

--- fp-go/tools/matmul/main.go
+++ fp-go/tools/matmul/main.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 	"fp-go/matrix"
 	"fp-go/util"
+	"time"
 )
 
-
-
 func main() {
+	start := time.Now()
 	A := util.ImportMat("A")
 	B := util.ImportMat("B")
+	endImport := time.Now()
 
 	C := matrix.Multiply(A, B)
-
-	fmt.Printf("C = %+v", C)
+	endMultiply := time.Now()
 
 	util.ExportMat("C", C)
+	endExport := time.Now()
+
+	durImport := endImport.Sub(start)
+	durMultiply := endMultiply.Sub(endImport)
+	durExport := endExport.Sub(endMultiply)
+
+	fmt.Printf("-- Timing --\n")
+	fmt.Printf("Import: %s\n", durImport)
+	fmt.Printf("Multiply: %s\n", durMultiply)
+	fmt.Printf("Export: %s\n", durExport)
+
+	//fmt.Printf("C = %+v", C)
 }
