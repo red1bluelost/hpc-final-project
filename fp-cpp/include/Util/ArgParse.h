b40e332c33bd75c2bc90d0709a3ef46bce5f2669
Micah Weston <msweston12@cox.net>
2022-04-01 22:40:01 -0400

    Updates with a flag variable.

---
+++ fp-cpp/include/Util/ArgParse.h
@@ -0,0 +1,11 @@
+#ifndef FP_CPP_UTIL_ARGPARSE_H
+#define FP_CPP_UTIL_ARGPARSE_H
+
+#include <algorithm>
+#include <string_view>
+
+inline constexpr bool argBool(const int Argc, const char *Argv[], std::string_view Flag) noexcept {
+  return std::find_if(Argv, Argv + Argc, [Flag](std::string_view SV) { return SV == Flag; });
+}
+
+#endif //FP_CPP_UTIL_ARGPARSE_H
--- fp-cpp/tools/matmul/main.cpp
+++ fp-cpp/tools/matmul/main.cpp
@@ -1,10 +1,13 @@
 #include "Matrix/Matrix.h"
 #include "Matrix/Multiply.h"
+#include "Util/ArgParse.h"
 
 #include <fstream>
 #include <iostream>
 
 int main(const int Argc, const char *Argv[]) {
+  bool Verify = argBool(Argc, Argv, "-v");
+
   std::ifstream AFile("A.mat"), BFile("B.mat");
   matrix::Dense A, B;
   AFile >> A;
@@ -15,7 +18,7 @@ int main(const int Argc, const char *Argv[]) {
   std::ofstream CFile("C.mat");
   CFile << C;
 
-  if (C != matrix::multiply(A, B)) {
+  if (Verify && C != matrix::multiply(A, B)) {
     std::cerr << "multiplication failed\n";
     return 1;
   }
