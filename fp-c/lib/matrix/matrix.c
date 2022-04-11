b9a2056849f2b2c62662e8c9eaf8913f08037f0f
Micah Weston <msweston12@cox.net>
2022-04-11 13:00:53 -0400

    Adds basic IO for the matrix operations.

---
+++ fp-c/lib/matrix/matrix.c
@@ -0,0 +1,41 @@
+#include "matrix/matrix.h"
+
+#include <malloc.h>
+
+extern inline void matrix_destroy(matrix_t* mat);
+extern inline int matrix_read(FILE* restrict stream, matrix_t* restrict mat);
+extern inline int matrix_write(FILE* restrict stream,
+                               const matrix_t* restrict mat);
+
+int matrix_read_r(FILE* restrict stream, double** restrict data,
+                  size_t* restrict rows, size_t* restrict cols) {
+  int ret;
+  double* data_in = NULL;
+  if ((ret = fscanf(stream, "%zu %zu\n", rows, cols)) != 2) goto error;
+
+  size_t size = (*rows) * (*cols);
+  data_in = malloc(size * sizeof(*data_in));
+  for (size_t i = 0; i < size; ++i) {
+    if ((ret = fscanf(stream, "%lf ", &data_in[i])) != 1) goto error;
+  }
+  *data = data_in;
+  return 0;
+error:
+  if (!data_in) free(data_in);
+  return ret;
+}
+
+int matrix_write_r(FILE* restrict stream, const double* restrict data,
+                   size_t rows, size_t cols) {
+  int ret;
+  if ((ret = fprintf(stream, "%zu %zu\n", rows, cols)) < 4) return ret;
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      size_t idx = i * cols + j;
+      double d = data[idx];
+      if ((ret = fprintf(stream, "%lf ", d) < 2)) return ret;
+    }
+    if ((ret = fprintf(stream, "\n") < 1)) return ret;
+  }
+  return 0;
+}
--- fp-c/tools/matmul/main.c
+++ fp-c/tools/matmul/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "matrix/matrix.h"
 
 int main() {
-  printf("Hello, World!\n");
+  FILE* afile;
+
+  if ((afile = fopen("A.mat", "r")) == NULL) {
+    perror("damn");
+    return EXIT_FAILURE;
+  }
+
+  matrix_t mat;
+  if (matrix_read(afile, &mat) != 0) {
+    perror("fuck");
+    return EXIT_FAILURE;
+  }
+
+  if (mat.rows != 3) {
+    printf("shit\n");
+    return EXIT_FAILURE;
+  }
+
+  if (mat.cols != 3) {
+    printf("bitch\n");
+    return EXIT_FAILURE;
+  }
+
+  matrix_write(stdout, &mat);
+  matrix_destroy(&mat);
+
   return 0;
 }
