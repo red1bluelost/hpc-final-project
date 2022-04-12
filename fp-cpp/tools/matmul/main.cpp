5cccfafa182566fb8f2230b3b2926fd93f8d3ffb
Micah Weston <msweston12@cox.net>
2022-04-11 23:07:41 -0400

    Adds threading for IO and buffers.

--- fp-cpp/tools/matmul/main.cpp
+++ fp-cpp/tools/matmul/main.cpp
@@ -5,23 +5,42 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <string_view>
+#include <thread>
 
 int main(const int Argc, const char *Argv[]) {
   std::ios_base::sync_with_stdio(false);
   bool Verify = argBool(Argc, Argv, "-v");
 
   auto Start = std::chrono::high_resolution_clock::now();
-  std::ifstream AFile("A.mat"), BFile("B.mat");
   matrix::Dense A, B;
-  AFile >> A;
-  BFile >> B;
+
+  {
+    auto FileThreadFunc = [](auto FName, matrix::Dense &Mat) {
+      std::ifstream F;
+      constexpr std::streamsize BufSize = 1 << 20;
+      char Buf[BufSize];
+      F.rdbuf()->pubsetbuf(Buf, BufSize);
+      F.open(FName);
+      F >> Mat;
+    };
+    std::thread AThread(FileThreadFunc, "A.mat", std::ref(A));
+    std::thread BThread(FileThreadFunc, "B.mat", std::ref(B));
+    AThread.join(), BThread.join();
+  }
   auto EndImport = std::chrono::high_resolution_clock::now();
 
   matrix::Dense C = matrix::multiply(A, B);
   auto EndMultiply = std::chrono::high_resolution_clock::now();
 
-  std::ofstream CFile("C.mat");
-  CFile << C;
+  {
+    std::ofstream CFile;
+    constexpr std::streamsize BufSize = 1 << 20;
+    char Buf[BufSize];
+    CFile.rdbuf()->pubsetbuf(Buf, BufSize);
+    CFile.open("C.mat");
+    CFile << C;
+  }
   auto EndExport = std::chrono::high_resolution_clock::now();
 
   {
